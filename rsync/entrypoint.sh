6874e1e7083bf59827079bdc2f1f8fc35de71788
Paul Matich <pnmatich@gmail.com>
2019-11-17 13:57:38 -0800

    Updated rsync container to use append mode

--- rsync/entrypoint.sh
+++ rsync/entrypoint.sh
@@ -5,6 +5,6 @@ piLogDirectory="/home/pi/Workshop/AquaBot/logs"
 
 while true; do
     echo "[INFO] - syncing logs from raspberrypi"
-    rsync -r -e "ssh -o StrictHostKeyChecking=no" --info=progress2 pi@${piAddress}:${piLogDirectory} /AquaBot/
-    sleep 10
+    rsync -r -e "ssh -o StrictHostKeyChecking=no" --append --info=progress2 pi@${piAddress}:${piLogDirectory} /AquaBot/
+    sleep 5
 done
