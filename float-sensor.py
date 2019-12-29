c7834a11d56d999b567697bb192f310c8e3c700f
Paul Matich <pnmatich@gmail.com>
2019-12-29 00:38:38 -0800

    Fixed float sensor

--- float-sensor.py
+++ float-sensor.py
@@ -7,8 +7,11 @@ GPIO.setmode(GPIO.BCM)
 # GPIO 23 & 17 set up as inputs. One pulled up, the other down.
 # 23 will go to GND when button pressed and 17 will go to 3V3 (3.3V)
 # this enables us to demonstrate both rising and falling edge detection
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+#GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+#GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+GPIO.setup(23, GPIO.IN)
+GPIO.setup(17, GPIO.IN)
 
 # now we'll define the threaded callback function
 # this will run in another thread when our event is detected
@@ -27,12 +30,14 @@ raw_input("Press Enter when ready\n>")
 # else is happening in the program, the function "my_callback" will be run
 # It will happen even while the program is waiting for
 # a falling edge on the other button.
-GPIO.add_event_detect(23, GPIO.BOTH, callback=edge_callback)
-GPIO.add_event_detect(17, GPIO.BOTH, callback=edge_callback)
+#GPIO.add_event_detect(23, GPIO.BOTH, callback=edge_callback)
+#GPIO.add_event_detect(17, GPIO.BOTH, callback=edge_callback)
 
 while True:
     time.sleep(1)
 
+    print "High Float: " + str(GPIO.input(17))
+    print "Low Float:" + str(GPIO.input(23))
 #    except KeyboardInterrupt:
 #        GPIO.cleanup()       # clean up GPIO on CTRL+C exit
 #    GPIO.cleanup()           # clean up GPIO on normal exit
