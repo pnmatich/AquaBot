7c5f16e729ad3aa8b47f453e55c04edfc6742bd8
Paul Matich <pmatich@pop-os.localdomain>
2021-01-27 21:51:36 -0800

    test

--- cron-gitops.sh
+++ cron-gitops.sh
@@ -3,6 +3,7 @@
 # go to the directory this script is being run from
 cd $(dirname $0)
 
-echo directory name: $(dirname $0) >> ~/gitops.log
+
+echo $(date) directory name: $(dirname $0) >> ~/.aquabot-gitops.log
 
 git pull origin master
