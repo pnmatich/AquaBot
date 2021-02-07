938841bc34995f8bb83bcdee2edf3c04cce37a9f
Paul Matich <pmatich@pop-os.localdomain>
2021-02-06 22:03:15 -0800

    added tagging

--- cron-gitops.sh
+++ cron-gitops.sh
@@ -2,8 +2,14 @@
 
 # go to the directory this script is being run from
 cd $(dirname $0)
-
+git pull origin master
 
 echo $(date) directory name: $(dirname $0) >> ~/.aquabot-gitops.log
 
 git pull origin master
+
+if [[ ! $(git describe --exact-match $(git rev-parse HEAD) 2>/dev/null) =~ ^ab.*$ ]]; then
+  tag_name="ab-$(date +%s)"
+  git tag -a "${tag_name}" -m "🌞 $(date --rfc-3339=s) - Successful deployment to aquabot"
+  git push origin ${tag_name}
+fi
