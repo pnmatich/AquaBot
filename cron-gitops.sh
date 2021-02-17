3f7c405c76a77a08b41b7240110410a16ed5daf0
Paul Matich <pmatich@pop-os.localdomain>
2021-02-16 22:06:44 -0800

    split up cron-gitops.sh

--- cron-gitops.sh
+++ cron-gitops.sh
@@ -3,14 +3,10 @@
 set -x
 
 # go to the directory this script is being run from
-cd $(dirname $0)
+aquabotDirectory="$(dirname $0)"
+cd ${aquabotDirectory}
 
 echo "[INFO] - $(date) directory name: $(dirname $0)" >> ~/.aquabot-gitops.log
-
 git pull origin master --rebase
 
-if [[ ! $(git describe --exact-match $(git rev-parse HEAD) 2>/dev/null) =~ ^ab.*$ ]]; then
-  tag_name="ab-$(date +"%Y-%m-%dT%H-%M-%S%Z")"
-  git tag -a "${tag_name}" -m "🌞 $(date +"%Y-%m-%dT%H:%M:%S%Z") - Successful aquabot deployment"
-  git push origin ${tag_name}
-fi
+${aquabotDirectory}/cron/main.sh
---
+++ main.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -x
+
+if [[ ! $(git describe --exact-match $(git rev-parse HEAD) 2>/dev/null) =~ ^ab.*$ ]]; then
+  tag_name="ab-$(date +"%Y-%m-%dT%H-%M-%S%Z")"
+  git tag -a "${tag_name}" -m "🌞 $(date +"%Y-%m-%dT%H:%M:%S%Z") - Successful aquabot deployment"
+  git push origin ${tag_name}
+fi
