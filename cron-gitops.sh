c187529a18b1844f062eebe23723f0960b0ca5fb
Paul Matich <pmatich@pop-os.localdomain>
2021-02-16 21:16:25 -0800

    fix

--- cron-gitops.sh
+++ cron-gitops.sh
@@ -11,6 +11,6 @@ git pull origin master --rebase
 
 if [[ ! $(git describe --exact-match $(git rev-parse HEAD) 2>/dev/null) =~ ^ab.*$ ]]; then
   tag_name="ab-$(date +"%Y-%m-%dT%H-%M-%S%Z")"
-  git tag -a "${tag_name}" -m "🌞 $(date +"%Y-%m-%dT%H:%M:%S%Z") - Successful deployment to aquabot"
+  git tag -a "${tag_name}" -m "🌞 $(date +"%Y-%m-%dT%H:%M:%S%Z") - Successful aquabot deployment"
   git push origin ${tag_name}
 fi
