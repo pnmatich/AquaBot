25b8a2e9e9a684b2dbe4bf996b1efb91c38631df
Paul Matich <pmatich@pop-os.localdomain>
2021-02-16 22:14:10 -0800

    tested new message in cron/main.sh

--- cron/main.sh
+++ cron/main.sh
@@ -4,6 +4,6 @@ set -x
 
 if [[ ! $(git describe --exact-match $(git rev-parse HEAD) 2>/dev/null) =~ ^ab.*$ ]]; then
   tag_name="ab-$(date +"%Y-%m-%dT%H-%M-%S%Z")"
-  git tag -a "${tag_name}" -m "🌞 $(date +"%Y-%m-%dT%H:%M:%S%Z") - Successful aquabot deployment"
+  git tag -a "${tag_name}" -m "🌞 $(date +"%Y-%m-%dT%H:%M:%S%Z") - Successful new message"
   git push origin ${tag_name}
 fi
