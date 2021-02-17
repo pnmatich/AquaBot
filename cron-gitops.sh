a26d1c9fc0579baa6d91073123b79053949f7aa8
Paul Matich <pmatich@pop-os.localdomain>
2021-02-16 23:10:05 -0800

    added git fetch to cron-gitops

--- cron-gitops.sh
+++ cron-gitops.sh
@@ -7,6 +7,7 @@ aquabotDirectory="$(dirname $0)"
 cd ${aquabotDirectory}
 
 echo "[INFO] - $(date) directory name: $(dirname $0)" >> ~/.aquabot-gitops.log
+git fetch origin
 git pull origin master --rebase
 
 ${aquabotDirectory}/cron/main.sh
