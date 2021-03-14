69a69ca73827504765a63a33b13668d8e82ac766
Paul Matich <pmatich@pop-os.localdomain>
2021-03-14 14:29:01 -0700

    set both float sensors to return 3 on high, and added restart aquabot service command

--- cron/main.sh
+++ cron/main.sh
@@ -6,4 +6,5 @@ if [[ ! $(git describe --exact-match $(git rev-parse HEAD) 2>/dev/null) =~ ^ab.*
   tag_name="ab-$(date +"%Y-%m-%dT%H-%M-%S%Z")"
   git tag -a "${tag_name}" -m "🌞 $(date +"%Y-%m-%dT%H:%M:%S%Z") - Successful remote deployment to aquabot"
   git push origin ${tag_name}
+  sudo service aquabot restart
 fi
