ecd91d6c76b10c7c1c77026a9b74f5af89e6425f
Paul Matich <pmatich@pop-os.localdomain>
2021-06-06 00:31:35 -0700

    at touch ~/aquabot.log to cron script

--- cron/main.sh
+++ cron/main.sh
@@ -7,4 +7,5 @@ if [[ ! $(git describe --exact-match $(git rev-parse HEAD) 2>/dev/null) =~ ^ab.*
   git tag -a "${tag_name}" -m "🌞 $(date +"%Y-%m-%dT%H:%M:%S%Z") - Successful remote deployment to aquabot"
   git push origin ${tag_name}
   sudo service aquabot restart
+  touch ~/.aquabot-log
 fi
