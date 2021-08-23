2a4bbf5af2b309fbe53ab9767428f45e38d655ff
Paul Matich <pmatich@pop-os.localdomain>
2021-08-22 23:29:30 -0700

    modified gitops entrypoint

--- cron-gitops.sh
+++ cron-gitops.sh
@@ -5,14 +5,21 @@
 # cron/main.sh is excecuted after changes are pulled down, so any
 # commands to add should be added there.
 
-
-
 # go to the directory this script is being run from
 aquabotDirectory="$(dirname $0)"
 cd ${aquabotDirectory}
 
 echo "[INFO] - $(date) directory name: $(dirname $0)" >> ~/.aquabot-gitops.log
-git fetch origin &>> ~/.aquabot-gitops.log
-git pull origin master &>> --rebase ~/.aquabot-gitops.log
 
+git fetch;
+LOCAL=$(git rev-parse HEAD);
+REMOTE=$(git rev-parse @{u});
+
+#if our local revision id doesn't match the remote, we will need to pull the changes
+if [ $LOCAL != $REMOTE ]; then
+    #pull and merge changes
+    git pull origin master;
+fi
+
+echo "[INFO] - $(date) running cron main.sh" >> ~/.aquabot-gitops.log
 ${aquabotDirectory}/cron/main.sh
