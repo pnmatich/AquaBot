fba114bdfbcaeb4f3550f36cc8800deae13942a3
Paul Matich <pmatich@pop-os.localdomain>
2021-08-29 23:10:38 -0700

    test

--- cron/main.sh
+++ cron/main.sh
@@ -17,6 +17,7 @@ if [[ ! $(git describe --exact-match $(git rev-parse HEAD) 2>/dev/null) =~ ^ab.*
   echo "[INFO] - creating tag ${tag_name}" >> ~/.aquabot-gitops.log
   git tag -a "${tag_name}" -m "🌞 $(date +"%Y-%m-%dT%H:%M:%S%Z") - Successful remote deployment to aquabot"
   git push origin ${tag_name}
+  echo "[INFO] - done pushing tag ${tag_name}" >> ~/.aquabot-gitops.log
   sudo service aquabot restart
   touch ~/.aquabot-log
 fi
