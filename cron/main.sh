02c70ae7f07006771976792c7c89c11dfd322e45
Paul Matich <pmatich@pop-os.localdomain>
2021-08-29 23:03:26 -0700

    moved tag info line

--- cron/main.sh
+++ cron/main.sh
@@ -13,8 +13,8 @@ fi
 if [[ ! $(git describe --exact-match $(git rev-parse HEAD) 2>/dev/null) =~ ^ab.*$ ]]; then
   echo "[INFO] - $(date) tagging commit" >> ~/.aquabot-gitops.log
 
-  echo "[INFO] - creating tag ${tag_name}" >> ~/.aquabot-gitops.log
   tag_name="ab-$(date +"%Y-%m-%dT%H-%M-%S%Z")"
+  echo "[INFO] - creating tag ${tag_name}" >> ~/.aquabot-gitops.log
   git tag -a "${tag_name}" -m "🌞 $(date +"%Y-%m-%dT%H:%M:%S%Z") - Successful remote deployment to aquabot"
   git push origin ${tag_name}
   sudo service aquabot restart
