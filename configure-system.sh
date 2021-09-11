e4c8b81c78bf6abe919ce738f5d5acd52ed036d8
Paul Matich <pmatich@pop-os.localdomain>
2021-09-10 20:10:02 -0700

    updated configure-system to configure git user

--- configure-system.sh
+++ configure-system.sh
@@ -4,3 +4,6 @@ mkdir -p ~/Workshop
 cd ~/Workshop
 rm -rf ./AquaBot
 git clone git@github.com:pnmatich/AquaBot.git
+cd ./AquaBot
+git config --global user.email "pnmatich@gmail.com"
+git config --global user.name "Paul Matich AquaBot Bravo"
