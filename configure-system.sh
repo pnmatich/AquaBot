4015ad561e5b4cb6ddcdc36380aafa639fe0a1cc
Paul Matich <pmatich@pop-os.localdomain>
2021-09-28 23:58:36 -0700

    added service creation to configure-system.sh

--- configure-system.sh
+++ configure-system.sh
@@ -9,12 +9,11 @@ cd ./AquaBot
 git config --global user.email "pnmatich@gmail.com"
 git config --global user.name "Paul Matich AquaBot Bravo"
 
+sudo cp pi/systemd/aquabot.service /lib/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable aquabot.service
 
-# sudo cp pi/systemd/aquabot.service /lib/systemd/system/
-# sudo systemctl daemon-reload
-# sudo systemctl enable aquabot.service
-
-# These files need to be copied
+# These files need to be copied to the pi
 # "./credentials/certificate.pem"
 # "./credentials/rootCA.pem"
 # "./credentials/private.key"
