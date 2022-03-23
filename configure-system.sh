1b8a9ea8ccbfa17e44e7d21aed396825b8de1b0b
Paul Matich AquaBot Bravo <pnmatich@gmail.com>
2022-03-22 17:11:07 -0700

    added rebase to configure system script

--- configure-system.sh
+++ configure-system.sh
@@ -5,6 +5,7 @@ mkdir -p ~/Workshop
 cd ~/Workshop
 git clone git@github.com:pnmatich/AquaBot.git
 cd ./AquaBot
+git config pull.rebase true
 git config --global user.email "pnmatich@gmail.com"
 git config --global user.name "Paul Matich AquaBot Bravo"
 
